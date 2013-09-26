5e5fac3fe46f18bd6c775e39c637ce27c80dbe98
Joe <hello.joer@gmail.com>
2013-09-26 15:12:36 +0100

    first commit

--- js/outlaw.js
+++ js/outlaw.js
@@ -1,11 +1,25 @@
+// Main toggle for image
 $(document).ready(function () {
-    $("#imgSmall").click(function () {
-        $("#imgSmall").toggleClass("imgLarge");
+    $('.imgSmall').click(function () {
+        $(this).toggleClass('imgSmall');
+        $(this).toggleClass('imgLarge');
     });
 });
 
-$('img').bind('touchstart', function (e) {
-    $(this).trigger('click');
-    e.preventDefault();
+// Centers image perfectly
+function resize() {
+    if ($(".imgSmall").height() <= $(window).height()) {
+        $(".imgSmall")
+            .height($(window).height())
+            .width($(window).height() * 1.5);
+    }
+
+    $(".imgSmall").center();
+}
+
+$(document).ready(function () {
+    resize();
 });
- 
+$(window).resize(function () {
+    resize();
+});
